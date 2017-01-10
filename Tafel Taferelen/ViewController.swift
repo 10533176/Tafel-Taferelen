d735805b7f3751b892b42b98700d7c265f3a4b05
10533176 <fvanson@live.nl>
2017-01-10 11:58:52 +0100

    Initial Commit

---
+++ Tafel Taferelen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tafel Taferelen
+//
+//  Created by Femke van Son on 10-01-17.
+//  Copyright © 2017 Femke van Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
