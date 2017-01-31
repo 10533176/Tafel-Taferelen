e9cb9f83ba6bae240c89469d0585d4f9baa01b20
10533176 <fvanson@live.nl>
2017-01-31 15:56:50 +0100

    cleaning hoofdmenu

--- Tafel Taferelen/HoofdmenuViewController.swift
+++ Tafel Taferelen/HoofdmenuViewController.swift
@@ -17,18 +17,10 @@ class HoofdmenuViewController: UIViewController {
     @IBOutlet weak var groupNameBtn: UIButton!
     @IBOutlet weak var noGroupBtn: UIButton!
     @IBOutlet weak var noDateBtn: UIButton!
-    
-    @IBOutlet weak var imageSeat1: UIImageView!
-    @IBOutlet weak var imageSeat2: UIImageView!
-    @IBOutlet weak var imageSeat3: UIImageView!
-    @IBOutlet weak var imageSeat4: UIImageView!
-    @IBOutlet weak var imageSeat5: UIImageView!
-    @IBOutlet weak var imageSeat6: UIImageView!
-    @IBOutlet weak var imageSeat7: UIImageView!
-    @IBOutlet weak var imageSeat8: UIImageView!
-    @IBOutlet weak var imageSeat9: UIImageView!
-    @IBOutlet weak var imageSeat10: UIImageView!
 
+    
+    @IBOutlet var plates: [UIImageView]!
+    
     var ref: FIRDatabaseReference!
     var tableSetting = [String]()
     var pfURL = String()
@@ -38,6 +30,7 @@ class HoofdmenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableSetting = [""]
         noGroupBtn.isHidden = true
 
@@ -57,8 +50,7 @@ class HoofdmenuViewController: UIViewController {
     }
     
     // MARK: Loading the user and group info to display 
-    
-    func loadProfilePicture() {
+    private func loadProfilePicture() {
         
         ref?.child("users").child(userID!).child("urlToImage").observeSingleEvent(of: .value, with: { (snapshot) in
             
@@ -79,7 +71,7 @@ class HoofdmenuViewController: UIViewController {
         self.pfPicture.clipsToBounds = true
     }
     
-    func getGroupInfo() {
+    private func getGroupInfo() {
         
         self.ref?.child("users").child(userID!).child("groupID").observeSingleEvent(of: .value, with: { (snapshot) in
             
@@ -97,7 +89,7 @@ class HoofdmenuViewController: UIViewController {
         })
     }
 
-    func getGroupName(groupID: String) {
+    private func getGroupName(groupID: String) {
         
         self.ref?.child("groups").child(groupID).child("name").observeSingleEvent(of: .value, with: { (snapshot) in
             let groupName = snapshot.value as! String
@@ -108,7 +100,7 @@ class HoofdmenuViewController: UIViewController {
         })
     }
     
-    func getGroupDate(groupID: String) {
+    private func getGroupDate(groupID: String) {
         
         self.ref?.child("groups").child(groupID).child("date").observeSingleEvent(of: .value, with: { (snapshot) in
             
@@ -122,7 +114,7 @@ class HoofdmenuViewController: UIViewController {
         })
     }
     
-    func getTableSetting(groupID: String) {
+    private func getTableSetting(groupID: String) {
         
         self.ref?.child("groups").child(groupID).child("tableSetting").observeSingleEvent(of: .value, with: { (snapshot) in
             let table = snapshot.value as? [String]
@@ -134,70 +126,27 @@ class HoofdmenuViewController: UIViewController {
     }
     
     // MARK: enabeling user to pick a seat at the table or remove current seat 
-    
-    @IBAction func seat1Pressed(_ sender: Any) {
-        saveSeat(seat: 0)
-    }
-    @IBAction func seat2Pressed(_ sender: Any) {
-        saveSeat(seat: 1)
-    }
-    @IBAction func seat3Pressed(_ sender: Any) {
-        saveSeat(seat: 2)
-    }
-    @IBAction func seat4Pressed(_ sender: Any) {
-        saveSeat(seat: 3)
-    }
-    @IBAction func seat5Pressed(_ sender: Any) {
-        saveSeat(seat: 4)
-    }
-    @IBAction func seat6Pressed(_ sender: Any) {
-        saveSeat(seat: 5)
-    }
-    @IBAction func seat7Pressed(_ sender: Any) {
-        saveSeat(seat: 6)
-    }
-    @IBAction func seat8Pressed(_ sender: Any) {
-        saveSeat(seat: 7)
-    }
-    @IBAction func seat9Pressed(_ sender: Any) {
-        saveSeat(seat: 8)
-    }
-    @IBAction func seat10Pressed(_ sender: Any) {
-        saveSeat(seat: 9)
+    @IBAction func clickedPlate(_ sender: UIButton) {
+        saveSeat(seat: sender.tag)
     }
     
-    
-    func saveSeat(seat: Int) {
+    private func saveSeat(seat: Int) {
         
-        let userID = FIRAuth.auth()?.currentUser?.uid
         if tableSetting.isEmpty == false {
             
             if self.tableSetting[seat] == "" {
-                self.seatClickedEmpty(seat: seat)
+                self.seatClickedIsOwn(seat: seat)
                 
             } else if self.tableSetting[seat] == self.pfURL {
-                self.tableSetting[seat] = ""
-                
-                self.ref?.child("users").child(userID!).child("groupID").observeSingleEvent(of: .value, with: { (snapshot) in
-                    
-                    let groupID = snapshot.value as? String
-                    
-                    if groupID != nil {
-                        self.ref?.child("groups").child(groupID!).child("tableSetting").setValue(self.tableSetting)
-                        self.emptyOwnPicture(seat: seat)
-                    }
-                })
-            } else if self.tableSetting[seat] != "" && self.tableSetting[seat] != self.pfURL {
-                //self.signupErrorAlert(title: "Oops!", message: "Pick an empty seat.")
+                self.seatClickedIsOwn(seat: seat)
             }
         }
 
     }
     
-    func seatClickedEmpty(seat: Int) {
+    private func seatClickedEmpty(seat: Int) {
         
         if self.tableSetting.contains(self.pfURL) {
-            
             self.signupErrorAlert(title: "Oops!", message: "You allready have a seat at the table!")
         } else {
             self.tableSetting[seat] = self.pfURL
@@ -214,162 +163,43 @@ class HoofdmenuViewController: UIViewController {
         }
     }
     
-    func displayOwnPicture(seat: Int) {
-    
-        if let url = NSURL(string: pfURL) {
+    private func seatClickedIsOwn(seat: Int) {
+        self.tableSetting[seat] = ""
+        
+        self.ref?.child("users").child(userID!).child("groupID").observeSingleEvent(of: .value, with: { (snapshot) in
             
-            if let data = NSData(contentsOf: url as URL) {
-                
-                if seat == 0 {
-                    self.imageSeat1.image = UIImage(data: data as Data)
-                    self.imageSeat1.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat1.clipsToBounds = true
-                }
-                if seat == 1 {
-                    self.imageSeat2.image = UIImage(data: data as Data)
-                    self.imageSeat2.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat2.clipsToBounds = true
-                }
-                if seat == 2 {
-                    self.imageSeat3.image = UIImage(data: data as Data)
-                    self.imageSeat3.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat3.clipsToBounds = true
-                }
-                if seat == 3 {
-                    self.imageSeat4.image = UIImage(data: data as Data)
-                    self.imageSeat4.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat4.clipsToBounds = true
-                }
-                if seat == 4 {
-                    self.imageSeat5.image = UIImage(data: data as Data)
-                    self.imageSeat5.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat5.clipsToBounds = true
-                }
-                if seat == 5 {
-                    self.imageSeat6.image = UIImage(data: data as Data)
-                    self.imageSeat6.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat6.clipsToBounds = true
-                }
-                if seat == 6 {
-                    self.imageSeat7.image = UIImage(data: data as Data)
-                    self.imageSeat7.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat7.clipsToBounds = true
-                }
-                if seat == 7 {
-                    self.imageSeat8.image = UIImage(data: data as Data)
-                    self.imageSeat8.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat8.clipsToBounds = true
-                }
-                if seat == 8 {
-                    self.imageSeat9.image = UIImage(data: data as Data)
-                    self.imageSeat9.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat9.clipsToBounds = true
-                }
-                if seat == 9 {
-                    self.imageSeat10.image = UIImage(data: data as Data)
-                    self.imageSeat10.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                    self.imageSeat10.clipsToBounds = true
-                }
+            let groupID = snapshot.value as? String
+            
+            if groupID != nil {
+                self.ref?.child("groups").child(groupID!).child("tableSetting").setValue(self.tableSetting)
+                self.plates[seat].image = nil
             }
-        }
+        })
     }
     
-    func emptyOwnPicture(seat: Int) {
+    private func filInTable() {
         
-        if seat == 0 {
-            self.imageSeat1.image = nil
-        }
-        if seat == 1 {
-            self.imageSeat2.image = nil
-        }
-        if seat == 2 {
-            self.imageSeat3.image = nil
-        }
-        if seat == 3 {
-            self.imageSeat4.image = nil
-        }
-        if seat == 4 {
-            self.imageSeat5.image = nil
-        }
-        if seat == 5 {
-            self.imageSeat6.image = nil
-        }
-        if seat == 6 {
-            self.imageSeat7.image = nil
-        }
-        if seat == 7 {
-            self.imageSeat8.image = nil
-        }
-        if seat == 8 {
-            self.imageSeat9.image = nil
-        }
-        if seat == 9 {
-            self.imageSeat10.image = nil
-        }
-
-    }
-    
-    func filInTable() {
-        
+        var indexTable = 0
         for key in tableSetting {
+            
             if key != "" {
                 if let url = NSURL(string: key) {
                     
                     if let data = NSData(contentsOf: url as URL) {
-                        
-                        if key == tableSetting[0] {
-                            self.imageSeat1.image = UIImage(data: data as Data)
-                            self.imageSeat1.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat1.clipsToBounds = true
-                        }
-                        if key == tableSetting[1] {
-                            self.imageSeat2.image = UIImage(data: data as Data)
-                            self.imageSeat2.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat2.clipsToBounds = true
-                        }
-                        if key == tableSetting[2] {
-                            self.imageSeat3.image = UIImage(data: data as Data)
-                            self.imageSeat3.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat3.clipsToBounds = true
-                        }
-                        if key == tableSetting[3] {
-                            self.imageSeat4.image = UIImage(data: data as Data)
-                            self.imageSeat4.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat4.clipsToBounds = true
-                        }
-                        if key == tableSetting[4] {
-                            self.imageSeat5.image = UIImage(data: data as Data)
-                            self.imageSeat5.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat5.clipsToBounds = true
-                        }
-                        if key == tableSetting[5] {
-                            self.imageSeat6.image = UIImage(data: data as Data)
-                            self.imageSeat6.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat6.clipsToBounds = true
-                        }
-                        if key == tableSetting[6] {
-                            self.imageSeat7.image = UIImage(data: data as Data)
-                            self.imageSeat7.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat7.clipsToBounds = true
-                        }
-                        if key == tableSetting[7] {
-                            self.imageSeat8.image = UIImage(data: data as Data)
-                            self.imageSeat8.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat8.clipsToBounds = true
-                        }
-                        if key == tableSetting[8] {
-                            self.imageSeat9.image = UIImage(data: data as Data)
-                            self.imageSeat9.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat9.clipsToBounds = true
-                        }
-                        if key == tableSetting[9] {
-                            self.imageSeat10.image = UIImage(data: data as Data)
-                            self.imageSeat10.layer.cornerRadius = self.imageSeat1.frame.size.width / 2
-                            self.imageSeat10.clipsToBounds = true
-                        }
+                        makeImageRound(indexTable: indexTable, key: key, data: data as Data)
                     }
                 }
             }
+            indexTable += 1
+        }
+    }
+    
+    private func makeImageRound(indexTable: Int, key: String, data: Data) {
+        if key == tableSetting[indexTable] {
+            
+            plates[indexTable].image = UIImage(data: data as Data)
+            plates[indexTable].layer.cornerRadius = self.plates[indexTable].frame.size.width / 2
+            plates[indexTable].clipsToBounds = true
         }
     }
     
